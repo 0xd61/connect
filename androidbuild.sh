c8f80a8dcf7b8e17a77d3f47393b4ea04cf2a495
Daniel Glinka <kaitsh@degit.co>
2021-02-08 10:39:27 -0300

    Initial setup

---
+++ androidbuild.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+SOURCES=( "android_main.cpp" )
+_SOURCES=()
+MKSOURCES=""
+SCRIPT=$(readlink -f "$0")
+CURDIR=$(dirname "$SCRIPT")
+SOURCE_FOLDER="$CURDIR/src"
+
+echo $CURDIR
+
+for src in ${SOURCES[@]}
+do
+    _SOURCES+="$SOURCE_FOLDER/$src"
+    MKSOURCES="$MKSOURCES $(basename $src)"
+done
+
+# Fetch sources
+# if [[ $# -ge 2 ]]; then
+#     for src in ${@:2}
+#     do
+#         SOURCES+=($src)
+#         MKSOURCES="$MKSOURCES $(basename $src)"
+#     done
+# else
+#     if [ -n "$1" ]; then
+#         while read src
+#         do
+#             SOURCES+=($src)
+#             MKSOURCES="$MKSOURCES $(basename $src)"
+#         done
+#     fi
+# fi
+
+# if [ -z "$1" ] || [ -z "$SOURCES" ]; then
+#     echo "Usage: androidbuild.sh com.yourcompany.yourapp < sources.list"
+#     echo "Usage: androidbuild.sh com.yourcompany.yourapp source1.c source2.c ...sourceN.c"
+#     echo "To copy SDL source instead of symlinking: COPYSOURCE=1 androidbuild.sh ... "
+#     exit 1
+# fi
+
+SDLVERSION="SDL2-2.0.14"
+SDLPATH="$CURDIR/$SDLVERSION"
+
+[ -d $SDLPATH ] || curl https://www.libsdl.org/release/$SDLVERSION.tar.gz | tar xzf - && mv $SDLVERSION $SDLPATH
+
+if [ -z "$ANDROID_HOME" ];then
+    echo "Please set the ANDROID_HOME directory to the path of the Android SDK"
+    exit 1
+fi
+
+if [ ! -d "$ANDROID_HOME/ndk-bundle" -a -z "$ANDROID_NDK_HOME" ]; then
+    echo "Please set the ANDROID_NDK_HOME directory to the path of the Android NDK"
+    exit 1
+fi
+
+APP="$1"
+APPARR=(${APP//./ })
+BUILDPATH="$CURDIR/build/$APP"
+
+# Start Building
+
+rm -rf $BUILDPATH
+mkdir -p $BUILDPATH
+
+cp -r $SDLPATH/android-project/* $BUILDPATH
+
+# Copy SDL sources
+mkdir -p $BUILDPATH/app/jni/SDL
+if [ -z "$COPYSOURCE" ]; then
+    ln -s $SDLPATH/src $BUILDPATH/app/jni/SDL
+    ln -s $SDLPATH/include $BUILDPATH/app/jni/SDL
+else
+    cp -r $SDLPATH/src $BUILDPATH/app/jni/SDL
+    cp -r $SDLPATH/include $BUILDPATH/app/jni/SDL
+fi
+
+cp -r $SDLPATH/Android.mk $BUILDPATH/app/jni/SDL
+sed -i -e "s|YourSourceHere.c|$MKSOURCES|g" $BUILDPATH/app/jni/src/Android.mk
+sed -i -e "s|org\.libsdl\.app|$APP|g" $BUILDPATH/app/build.gradle
+sed -i -e "s|org\.libsdl\.app|$APP|g" $BUILDPATH/app/src/main/AndroidManifest.xml
+
+# Copy user sources
+# for src in "${_SOURCES[@]}"
+# do
+#     cp $src $BUILDPATH/app/jni/src
+# done
+if [ -z "$COPYSOURCE" ]; then
+    ln -s $SOURCE_FOLDER/* $BUILDPATH/app/jni/src
+else
+    cp -r $SOURCE_FOLDER/* $BUILDPATH/app/jni/src
+fi
+
+# Create an inherited Activity
+cd $BUILDPATH/app/src/main/java
+for folder in "${APPARR[@]}"
+do
+    mkdir -p $folder
+    cd $folder
+done
+
+ACTIVITY="${folder}Activity"
+sed -i -e "s|\"SDLActivity\"|\"$ACTIVITY\"|g" $BUILDPATH/app/src/main/AndroidManifest.xml
+
+# Fill in a default Activity
+cat >"$ACTIVITY.java" <<__EOF__
+package $APP;
+
+import org.libsdl.app.SDLActivity;
+
+public class $ACTIVITY extends SDLActivity
+{
+}
+__EOF__
+
+# Update project and build
+#echo "To build and install to a device for testing, run the following:"
+
+echo "Building: $BUILDPATH"
+pushd $BUILDPATH > /dev/null
+
+./gradlew installDebug
+
+popd > /dev/null
+
+echo "Build directory: $BUILDPATH"
---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Stop at errors
+set -e
+
+CommonCompilerFlags="-O3 -g -ggdb -fdiagnostics-color=always -std=c++11 -fno-rtti -fno-exceptions -ffast-math -msse4.1
+-Wall -Werror -Wconversion
+-Wno-writable-strings -Wno-gnu-anonymous-struct
+-Wno-padded -Wno-string-conversion
+-Wno-error=sign-conversion -Wno-incompatible-function-pointer-types
+-Wno-error=unused-variable -Wno-unused-function
+-Wno-error=unused-command-line-argument"
+
+CommonDefines="-DZHC_DEBUG=1 -DZHC_INTERNAL=1 -DOS_NAME=${OS_NAME}"
+
+# the goal should be -nostdlib
+CommonLinkerFlags="-Wl,--gc-sections -lm"
+
+if [ -z "$1" ]; then
+    OS_NAME=$(uname -o 2>/dev/null || uname -s)
+else
+    OS_NAME="$1"
+fi
+
+echo "Building for $OS_NAME..."
+
+curDir=$(pwd)
+srcDir="$curDir/src"
+buildDir="$curDir/build"
+dataDir="$curDir/data"
+
+[ -d $buildDir ] || mkdir -p $buildDir
+[ -d $dataDir ] || mkdir -p $dataDir
+
+pushd $buildDir > /dev/null
+
+# NOTE(dgl): currently only x64 code. For other architectures we have to adjust the intrinsics.
+if [ "$OS_NAME" == "GNU/Linux" ] || \
+   [ "$OS_NAME" == "Linux" ] || \
+   [ "$OS_NAME" == "linux" ]; then
+    # PIC = Position Independent Code
+    # -lm -> we have to link the math library...
+    clang $CommonCompilerFlags $CommonDefines $CommonLinkerFlags -o linux_main_x64 $srcDir/linux_main.cpp \
+    `sdl2-config --static-libs`
+elif [ "$OS_NAME" == "Android" ] || \
+     [ "$OS_NAME" == "android" ]; then
+    # TODO(dgl): Android build not yet tested!
+    $curDir/androidbuild.sh "co.degit.connect"
+else
+    echo "$OS_NAME is currently not supported"
+fi
+
+popd > /dev/null
