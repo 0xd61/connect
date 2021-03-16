37e068e77c441008dba886afb5d7f58026e66b94
Daniel Glinka <kaitsh@degit.co>
2021-03-16 13:34:56 -0300

    Use android studio for the android build

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # Stop at errors
 set -e
 
-CommonCompilerFlags="-O0 -g -ggdb -fdiagnostics-color=always -std=c++11 -fno-rtti -fno-exceptions -ffast-math -msse4.1 -msse2
+NO_RELEASE=1
+
+CommonCompilerFlags="-O3 -g -ggdb -fdiagnostics-color=always -std=c++11 -fno-rtti -fno-exceptions -ffast-math -msse4.1 -msse2
 -Wall -Werror -Wconversion
 -Wno-writable-strings -Wno-gnu-anonymous-struct
 -Wno-padded -Wno-string-conversion
@@ -11,7 +13,7 @@ CommonCompilerFlags="-O0 -g -ggdb -fdiagnostics-color=always -std=c++11 -fno-rtt
 -Wno-error=unused-variable -Wno-unused-function
 -Wno-error=unused-command-line-argument"
 
-CommonDefines="-DZHC_BIG_ENDIAN=0 -DZHC_DEBUG=1 -DZHC_INTERNAL=1 -DOS_NAME=${OS_NAME}"
+CommonDefines="-DZHC_BIG_ENDIAN=0 -DZHC_DEBUG=${NO_RELEASE} -DZHC_INTERNAL=${NO_RELEASE} -DOS_NAME=${OS_NAME}"
 
 # the goal should be -nostdlib
 # TODO(dgl): link sdl2_net statically
@@ -65,8 +67,31 @@ if [ "$OS_NAME" == "GNU/Linux" ] || \
     cp -r $dataDir/assets/* linux/
 elif [ "$OS_NAME" == "Android" ] || \
      [ "$OS_NAME" == "android" ]; then
-    # TODO(dgl): Android build not yet tested!
-    $curDir/_androidbuild.sh "co.degit.connect"
+
+    SDLVERSION="SDL2-2.0.14"
+    SDLPATH="$curDir/$SDLVERSION"
+    SDLNETVERSION="SDL2_net-2.0.1"
+    SDLNETPATH="$curDir/$SDLNETVERSION"
+    ANDROID_PATH="$curDir/sdl2_android"
+
+    [ -d $SDLPATH ] || curl https://www.libsdl.org/release/$SDLVERSION.tar.gz | tar xzf -
+    [ -d $SDLVERSION ] && mv $SDLVERSION $SDLPATH
+
+    [ -d $SDLNETPATH ] || curl https://libsdl.org/projects/SDL_net/release/$SDLNETVERSION.tar.gz | tar xzf -
+    [ -d $SDLNETVERSION ] && mv $SDLNETVERSION $SDLNETPATH
+
+    mkdir -p $ANDROID_PATH/app/jni/SDL
+    ln -sfn $SDLPATH/Android.mk $ANDROID_PATH/app/jni/SDL/Android.mk
+    ln -sfn $SDLPATH/src $ANDROID_PATH/app/jni/SDL/src
+    ln -sfn $SDLPATH/include $ANDROID_PATH/app/jni/SDL/include
+    ln -sfn $SDLNETPATH $ANDROID_PATH/app/jni/SDLNet
+    ln -sfn $SDLPATH/android-project/app/src/main/java/org $ANDROID_PATH/app/src/main/java/org
+    mkdir -p $ANDROID_PATH/app/jni/src
+    ln -sfn $srcDir/* $ANDROID_PATH/app/jni/src/
+    ln -sfn $curDir/Android.mk $ANDROID_PATH/app/jni/src/Android.mk
+    ln -sfn $dataDir/assets $ANDROID_PATH/app/src/main/
+
+    echo "Created symlinks to sources. Please use android studio for the build"
 else
     echo "$OS_NAME is currently not supported"
 fi
---
+++ sdl2_android/app/src/main/java/co/degit/connect/MainActivity.java
@@ -0,0 +1,7 @@
+package co.degit.connect;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity
+{
+}
